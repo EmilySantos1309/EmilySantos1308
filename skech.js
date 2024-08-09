f606c68f96f2f9b2acb23fac0da080485fc35920
EmilySantos1309 <00001108939399sp@al.educacao.sp.gov.br>
2024-08-09 14:41:00 -0300

    adicionando arquivos js e css

---
+++ skech.js
@@ -0,0 +1,12 @@
+function setup() {
+  createCanvas(400, 400);
+  background("lightgrey")
+  
+  textFont("Comic Sans MS");
+  textSize(30)
+  textAlign(CENTER,CENTER);
+}
+
+function draw() {
+  text("Apredendo JS na Alura",200,200)
+}
